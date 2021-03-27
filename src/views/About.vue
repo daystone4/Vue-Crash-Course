20ffc9ce958238fe1f9f480e0feaef5b10a51a74
David Stone <daystone4@gmail.com>
2021-03-27 16:15:16 -0400

    Last Commit

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="about-container">
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Back to home</router-link>
+</div>
+    
+</template>
+
+<style scoped>
+.about-container {
+    display: grid;
+    place-items: center ;
+}
+</style>
